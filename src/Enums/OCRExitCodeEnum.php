177369c4a48846e44f2aa74c069acaf3d5fec3a6
Manuel Guzman <manuelguzman.villar@gmail.com>
2025-04-25 12:55:42 -0400

    Set correct exit code for time out

--- src/Enums/OCRExitCodeEnum.php
+++ src/Enums/OCRExitCodeEnum.php
@@ -8,6 +8,6 @@ enum OCRExitCodeEnum: int
     case PARSED_PARTIALLY = 2;
     case PARSED_FAILURE = 3;
     case FATAL_ERROR = 4;
+    case TIME_OUT = 6;
     case VALIDATION_ERROR = 99;
-    case TIME_OUT = 100;
 }
